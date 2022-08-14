67625a4373a1d65b6448009d2831947399e9f5bd
Yohei Kokubo <yhei.register@gmail.com>
2022-08-14 21:47:38 +0900

    不要箇所削除 - NW-23-mint機能を作る

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,11 +9,6 @@ module.exports = {
         count: 100,
         accountsBalance: "10000000000000000000000"
       },
-      namedAccounts: {
-        deployer: {
-            default: 0, // here this will by default take the first account as deployer
-        },
-      },
     }
   }
 };
