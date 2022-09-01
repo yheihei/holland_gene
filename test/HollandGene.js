75774dd3c3af5b2e52b3651fb03e93bf04839e36
Yohei Kokubo <yhei.register@gmail.com>
2022-09-01 23:19:49 +0900

    テスト変数名リファクタ - NW-37-preセール、pubセールの分岐を作る

--- test/HollandGene.js
+++ test/HollandGene.js
@@ -10,23 +10,23 @@ describe("HollandGene contract", function () {
     const HollandGene = await ethers.getContractFactory("HollandGene");
     const [owner, addr1, addr2, addr3] = await ethers.getSigners();
 
-    const hardhatToken = await HollandGene.deploy(
+    const nftContract = await HollandGene.deploy(
       'HollandGene',
       'HG',
       'ipfs://CID/',
       'ipfs://notRevealedUri'
     );
-    await hardhatToken.deployed();
+    await nftContract.deployed();
     // public saleにしておく
-    await hardhatToken.setPhase(2)
+    await nftContract.setPhase(2)
 
-    return { HollandGene, hardhatToken, owner, addr1, addr2, addr3 };
+    return { HollandGene, nftContract, owner, addr1, addr2, addr3 };
   }
 
   it("mintしたらNFTがmint数分取得できていること", async function () {
-    const { hardhatToken, addr1 } = await loadFixture(deployTokenFixture);
-    await hardhatToken.connect(addr1).mint(3, { value: ethers.utils.parseEther("1") });
-    const tokenIds = await hardhatToken.tokensOfOwner(addr1.address);
+    const { nftContract, addr1 } = await loadFixture(deployTokenFixture);
+    await nftContract.connect(addr1).mint(3, { value: ethers.utils.parseEther("1") });
+    const tokenIds = await nftContract.tokensOfOwner(addr1.address);
     expect(tokenIds).to.deep.equal(
       [
         ethers.BigNumber.from("1"),
@@ -37,12 +37,12 @@ describe("HollandGene contract", function () {
   });
 
   it("あるアカウントの所持tokenIdが飛び飛びになっていてもtokensOfOwnerでtokenIdのリストがとれること", async function () {
-    const { hardhatToken, addr1, addr2 } = await loadFixture(deployTokenFixture);
+    const { nftContract, addr1, addr2 } = await loadFixture(deployTokenFixture);
     // addr1の所持tokenIdが飛び飛びになるようmint
-    await hardhatToken.connect(addr1).mint(1, { value: ethers.utils.parseEther("1") });
-    await hardhatToken.connect(addr2).mint(2, { value: ethers.utils.parseEther("1") });
-    await hardhatToken.connect(addr1).mint(1, { value: ethers.utils.parseEther("1") });
-    expect(await hardhatToken.tokensOfOwner(addr1.address)).to.deep.equal(
+    await nftContract.connect(addr1).mint(1, { value: ethers.utils.parseEther("1") });
+    await nftContract.connect(addr2).mint(2, { value: ethers.utils.parseEther("1") });
+    await nftContract.connect(addr1).mint(1, { value: ethers.utils.parseEther("1") });
+    expect(await nftContract.tokensOfOwner(addr1.address)).to.deep.equal(
       [
         ethers.BigNumber.from("1"),
         ethers.BigNumber.from("4"),
@@ -51,58 +51,58 @@ describe("HollandGene contract", function () {
   });
 
   it("ownerは0ethでmintできること", async function () {
-    const { hardhatToken, owner } = await loadFixture(deployTokenFixture);
-    await hardhatToken.connect(owner).mint(1, { value: ethers.utils.parseEther("0") });
-    const tokenIds = await hardhatToken.tokensOfOwner(owner.address);
+    const { nftContract, owner } = await loadFixture(deployTokenFixture);
+    await nftContract.connect(owner).mint(1, { value: ethers.utils.parseEther("0") });
+    const tokenIds = await nftContract.tokensOfOwner(owner.address);
     expect(tokenIds).to.deep.equal(
       [ ethers.BigNumber.from("1") ]
     );
   });
 
   it("Maxの供給量をpublic関数でセットできること", async function () {
-    const { hardhatToken } = await loadFixture(deployTokenFixture);
-    await hardhatToken.setMaxSupply(100);
-    expect(await hardhatToken.maxSupply()).to.equal(
+    const { nftContract } = await loadFixture(deployTokenFixture);
+    await nftContract.setMaxSupply(100);
+    expect(await nftContract.maxSupply()).to.equal(
       ethers.BigNumber.from("100")
     );
   });
 
   it("Maxの供給量のset関数がowner以外だとエラーになること", async function () {
-    const { hardhatToken, addr1 } = await loadFixture(deployTokenFixture);
-    await expect(hardhatToken.connect(addr1).setMaxSupply(100)).to.be.revertedWith(
+    const { nftContract, addr1 } = await loadFixture(deployTokenFixture);
+    await expect(nftContract.connect(addr1).setMaxSupply(100)).to.be.revertedWith(
       'Ownable: caller is not the owner'
     )
   });
 
   it("revealedがtrueの時、mintしたらリビール前の情報が取得できること", async function () {
-    const { hardhatToken, addr1 } = await loadFixture(deployTokenFixture);
-    await hardhatToken.setRevealed(true)
-    await hardhatToken.connect(addr1).mint(1, { value: ethers.utils.parseEther("1") });
-    const tokenURI = await hardhatToken.tokenURI(1)
+    const { nftContract, addr1 } = await loadFixture(deployTokenFixture);
+    await nftContract.setRevealed(true)
+    await nftContract.connect(addr1).mint(1, { value: ethers.utils.parseEther("1") });
+    const tokenURI = await nftContract.tokenURI(1)
     expect(tokenURI).to.equal('ipfs://CID/1.json')
   });
 
   it("revealedがfalseの時、mintしたらリビール前の情報が取得できること", async function () {
-    const { hardhatToken, addr1 } = await loadFixture(deployTokenFixture);
-    await hardhatToken.setRevealed(false)
-    await hardhatToken.connect(addr1).mint(1, { value: ethers.utils.parseEther("1") });
-    const tokenURI = await hardhatToken.tokenURI(1)
+    const { nftContract, addr1 } = await loadFixture(deployTokenFixture);
+    await nftContract.setRevealed(false)
+    await nftContract.connect(addr1).mint(1, { value: ethers.utils.parseEther("1") });
+    const tokenURI = await nftContract.tokenURI(1)
     expect(tokenURI).to.equal('ipfs://notRevealedUri')
   });
 
   it("WL所持済みのアドレスがwlMintできること", async function () {
-    const { hardhatToken, addr1, addr2 } = await loadFixture(deployTokenFixture);
+    const { nftContract, addr1, addr2 } = await loadFixture(deployTokenFixture);
     // WL saleにしておく
-    await hardhatToken.setPhase(1);
+    await nftContract.setPhase(1);
     const leaves = [addr1.address, addr2.address].map((x) =>
       keccak256(x)
     )
     // WL登録
     const tree = new MerkleTree(leaves, keccak256, { sortPairs: true })
     rootTree = tree.getRoot()
-    await hardhatToken.setMerkleRoot(rootTree)
+    await nftContract.setMerkleRoot(rootTree)
 
-    await hardhatToken.connect(addr1).wlMint(
+    await nftContract.connect(addr1).wlMint(
       1,
       /**
        * そのアドレスのproofはMerkleTreeを持った外部に問い合わせてもいいし、
@@ -111,26 +111,26 @@ describe("HollandGene contract", function () {
       tree.getHexProof(keccak256(addr1.address)),
       { value: ethers.utils.parseEther("1") }
     );
-    const tokenIds = await hardhatToken.tokensOfOwner(addr1.address);
+    const tokenIds = await nftContract.tokensOfOwner(addr1.address);
     expect(tokenIds).to.deep.equal(
       [ ethers.BigNumber.from("1") ]
     );
   });
 
   it("WL未所持のアドレスがwlMintできないこと", async function () {
-    const { hardhatToken, addr1, addr2, addr3 } = await loadFixture(deployTokenFixture);
+    const { nftContract, addr1, addr2, addr3 } = await loadFixture(deployTokenFixture);
     // WL saleにしておく
-    await hardhatToken.setPhase(1);
+    await nftContract.setPhase(1);
     const leaves = [addr1.address, addr2.address].map((x) =>
       keccak256(x)
     )
     // WL登録
     const tree = new MerkleTree(leaves, keccak256, { sortPairs: true })
     rootTree = tree.getRoot()
-    await hardhatToken.setMerkleRoot(rootTree)
+    await nftContract.setMerkleRoot(rootTree)
 
     await expect(
-      hardhatToken.connect(addr3).wlMint(
+      nftContract.connect(addr3).wlMint(
         1,
         tree.getHexProof(keccak256(addr3.address)),
         { value: ethers.utils.parseEther("1") }
@@ -139,27 +139,27 @@ describe("HollandGene contract", function () {
   });
 
   it("PublicSaleでない場合mint関数がエラーとなること", async function () {
-    const { hardhatToken, addr1 } = await loadFixture(deployTokenFixture);
+    const { nftContract, addr1 } = await loadFixture(deployTokenFixture);
     // sale前にしておく
-    await hardhatToken.setPhase(0);
+    await nftContract.setPhase(0);
     await expect(
-      hardhatToken.connect(addr1).mint(3, { value: ethers.utils.parseEther("1") })
+      nftContract.connect(addr1).mint(3, { value: ethers.utils.parseEther("1") })
     ).to.revertedWith('Public mint is not active.');
   })
 
   it("WLSaleでない場合wlMint関数がエラーとなること", async function () {
-    const { hardhatToken, addr1, addr2 } = await loadFixture(deployTokenFixture);
+    const { nftContract, addr1, addr2 } = await loadFixture(deployTokenFixture);
     // sale前にしておく
-    await hardhatToken.setPhase(0);
+    await nftContract.setPhase(0);
     const leaves = [addr1.address, addr2.address].map((x) =>
       keccak256(x)
     )
     // WL登録
     const tree = new MerkleTree(leaves, keccak256, { sortPairs: true })
     rootTree = tree.getRoot()
-    await hardhatToken.setMerkleRoot(rootTree)
+    await nftContract.setMerkleRoot(rootTree)
 
-    await expect(hardhatToken.connect(addr1).wlMint(
+    await expect(nftContract.connect(addr1).wlMint(
       1,
       tree.getHexProof(keccak256(addr1.address)),
       { value: ethers.utils.parseEther("1") }
